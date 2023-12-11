373bcaf5e9aa7ff444cfe49a1249c9c4ce75a952
Ying hao <z112517z@gmail.com>
2023-12-11 16:25:15 +0800

    feat:Queue Demo Trigger
    調整項目:
    1.新增接收queue Message 後要觸發的function

---
+++ src/main/java/com/howhow/functions/handler/queue/FinalDemoQueueHandler.java
@@ -0,0 +1,20 @@
+package com.howhow.functions.handler.queue;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.annotation.BindingName;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.QueueTrigger;
+import com.microsoft.azure.functions.annotation.StorageAccount;
+import java.util.logging.Logger;
+
+public class FinalDemoQueueHandler {
+  @StorageAccount("AzureWebJobsStorage")
+  @FunctionName("asyncCreateMember")
+  public void asyncGetQueueMsg(
+      @BindingName("Id") String messageId,
+      @QueueTrigger(name = "message", queueName = "") String message,
+      final ExecutionContext context) {
+    Logger logger = context.getLogger();
+    logger.info("receive Message. id: " + messageId + ", message: " + message);
+  }
+}
